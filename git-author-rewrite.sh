2516e82f9407d9c089724587c71ca8423abcfaa7
Omer Dagan <omerx.dagan@intel.com>
2017-05-17 12:43:44 +0300

    change author of a git repository
    Change-Id: I55bcd7d5d5b5381e237a8e0cda3218739a2b3045

---
+++ git-author-rewrite.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="ramon.fried@gmail.com"
+CORRECT_NAME="Omer Dagan"
+CORRECT_EMAIL="omerdagan84@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
